0a0b47d4553bc767cda6200ba80987e4f4b50e2e
Pratik Warade <148488693+WaradePratik@users.noreply.github.com>
2024-05-02 18:27:44 +0530

    Update AddressDao.java

--- src/main/java/com/app/dao/AddressDao.java
+++ src/main/java/com/app/dao/AddressDao.java
@@ -10,14 +10,5 @@ import com.app.entities.Address;
 import com.app.entities.User;
 
 public interface AddressDao extends JpaRepository<Address, Long> {
-//	//list all emps by specified city (eg of custom query)
-//	//if you give wrong property name instead of a.owner : it will b detected as run time err
-//		@Query("select a.owner from Address a where a.city=:city")
-//		List<User> findEmployeesByCity(String city);
-//		//delete all addresses from specific state , rets no of rows
-//		long deleteByState(String state);//will cause select + n delete queries
-//		//OR : bulk deletion approach
-//		@Query("delete from Address a where a.state=:state")
-//		@Modifying
-//		long deleteAddressDetailsByState(String state);
+
 }
